da1167437d2460be21ba8f3d737065bf7eff9869
shockolada <shockolada@gmail.com>
2019-10-28 03:10:22 +0200

    27-10

---
+++ source/js/all.js
@@ -0,0 +1,7 @@
+@@include('./libs/jquery-3.4.1.js')
+@@include('./libs/svgxuse.min.js')
+@@include('./libs/jquery.lettering.js')
+@@include('./libs/jquery.fancybox.js')
+@@include('./libs/wow.js')
+@@include('./libs/swiper.js')
+@@include('./main.js')
